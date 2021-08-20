6462b5afda40904bd9ec2d089fd6af29b846b866
Ashutosh Sameer <ashutoshsameer@iitkgp.ac.in>
2021-08-20 15:09:47 -0400

    Update site

---
+++ precache-manifest.7568b27f494a2af7020aaa1cb9b20709.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "10feaff4ca67a862e3271b83d6fa736c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e8d730aa77d4f8cf6906",
+    "url": "/static/css/3.00373544.chunk.css"
+  },
+  {
+    "revision": "5dbca85ebd5903f06396",
+    "url": "/static/css/4.3e7d09e6.chunk.css"
+  },
+  {
+    "revision": "628669801f93a5390269",
+    "url": "/static/css/main.b028930d.chunk.css"
+  },
+  {
+    "revision": "52f18a30a3f2323ac478",
+    "url": "/static/js/2.c3f2079d.chunk.js"
+  },
+  {
+    "revision": "b9941b26ce7f87a25d1a3bc0d5ae97bc",
+    "url": "/static/js/2.c3f2079d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e8d730aa77d4f8cf6906",
+    "url": "/static/js/3.fc849642.chunk.js"
+  },
+  {
+    "revision": "5dbca85ebd5903f06396",
+    "url": "/static/js/4.dde4210f.chunk.js"
+  },
+  {
+    "revision": "628669801f93a5390269",
+    "url": "/static/js/main.442fd350.chunk.js"
+  },
+  {
+    "revision": "26036b15f318fe3da7b1",
+    "url": "/static/js/runtime-main.b0dc6bf9.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "13e4769a04b23328e5c473f0e6078a6c",
+    "url": "/static/media/IIT_Kharagpur_Logo.13e4769a.jpeg"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "cc96395e138a5dadbd9162bdf09dd722",
+    "url": "/static/media/Shape-Goldman-Sachs-Logo.cc96395e.png"
+  },
+  {
+    "revision": "47a7a717679b640ceb1b05d38c480a61",
+    "url": "/static/media/codeInLogo.47a7a717.webp"
+  },
+  {
+    "revision": "59389695208d7454c6607bed51ed4316",
+    "url": "/static/media/developerActivity.59389695.svg"
+  },
+  {
+    "revision": "e79c639294c805688be731921368c8f8",
+    "url": "/static/media/googleAssistantLogo.e79c6392.webp"
+  },
+  {
+    "revision": "6a75ae3cab325e84b70c6cc6fae0d969",
+    "url": "/static/media/img.6a75ae3c.png"
+  },
+  {
+    "revision": "52be31441c8cf8a8a16bd7a228d428bf",
+    "url": "/static/media/manOnTable.52be3144.svg"
+  },
+  {
+    "revision": "829ad0a1deef4d7295adfecdf12df2be",
+    "url": "/static/media/nextuLogo.829ad0a1.webp"
+  },
+  {
+    "revision": "ee62cb58630f29a6e6201b49d4c03728",
+    "url": "/static/media/pwaLogo.ee62cb58.webp"
+  },
+  {
+    "revision": "25018d29d5ab115bc51c3e9a2d92f90f",
+    "url": "/static/media/saayaHealthLogo.25018d29.webp"
+  },
+  {
+    "revision": "3440939881da8339d1597e8d77cdf850",
+    "url": "/static/media/skill.34409398.svg"
+  },
+  {
+    "revision": "13780cdd0144cacdef25486e2c2503f8",
+    "url": "/static/media/talksCardBack.13780cdd.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.1f70cc6b26ed9d89b9eb9b5a329cf1d2.js"
+  "/precache-manifest.7568b27f494a2af7020aaa1cb9b20709.js"
 );
 
 self.addEventListener('message', (event) => {
