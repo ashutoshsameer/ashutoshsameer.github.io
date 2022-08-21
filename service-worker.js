6c82f8880536b2dcd24be670b8c60f2ea5b573ef
ashutoshsameer <sameerashutosh0201@gmail.com>
2022-08-21 11:42:30 -0700

    Update site

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.3960f74e385820e0843731580d4cf462.js"
+  "/precache-manifest.2a00990f7fdcffbf579d844845c4eef4.js"
 );
 
 self.addEventListener('message', (event) => {
